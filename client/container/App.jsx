cf5216829227d4eabb424d82117d65cb6147b2d0
LindaHuangzixia <hzx1625759939@gmail.com>
2023-06-19 11:42:23 +0800

    add wholelist on the left navigation

--- client/container/App.jsx
+++ client/container/App.jsx
@@ -6,14 +6,23 @@ import Article from "../component/Article";
 const App = ({wholelists, article, positionSummary}) => {
     let [wholeGoups, setWholeGoups] = useState([]);
     useEffect(()=>{
-
+        let groups = wholelists.map(x=>x.Header).reduce((prev,cur) => prev.includes(cur) ? prev : [...prev,cur],[]);
+        let wholeGoupList = [];
+        groups.forEach(header=>{
+            let groupItems = (wholelists || []).filter(x=>x.Header == header);
+            wholeGoupList.push({
+                header: header,
+                items: groupItems
+            });
+        });
+        setWholeGoups(wholeGoupList);
     }, [wholelists]);
     return (
         <div className="container h-100">
             <div className="flex flex-row">
                 <div className="basis-1/3 mr-2">
                 <div className="text-xl/[26px] text-blue mb-3">LEADERS LIST</div>
-                    <div className="flex text-white">
+                    <div className="flex text-white hover:cursor-pointer">
                         <div className="bg-primary text-lg px-5 py-2">
                             Nvidia Shines, CELH Lags With Leaders Mixed After Fed
                         </div>
@@ -21,11 +30,11 @@ const App = ({wholelists, article, positionSummary}) => {
                             ...
                         </div>
                     </div>
-                    <div className="border border-[#ddd] text-lg shadow-md mt-4 p-3 flex">
+                    <div className="border border-[#ddd] text-lg shadow-md mt-4 p-3 flex hover:cursor-pointer rounded">
                         <UnorderedListOutlined className="mt-1"/>
                         <span className="pl-2">POSITIONS SUMMARY</span>
                     </div>
-                    <WholeList />
+                    <WholeList wholeGoups={wholeGoups} />
                 </div>
                 <div className="basis-2/3"> 
                     <Article article={article} />
