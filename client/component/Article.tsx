f48d90b36fd4c0292728f349f9bdb40aa089e87b
LindaHuangzixia <hzx1625759939@gmail.com>
2023-06-20 14:41:07 +0800

    update style for article page

--- client/component/Article.tsx
+++ client/component/Article.tsx
@@ -1,14 +1,20 @@
 import articleDataType from "../dataType/articleDataType";
 
 const Article = ({article}: {article: articleDataType}) =>{
-    return (<div className="mt-9 bg-white">
+    return (
+    <div className="bg-white border border-[#ddd] mt-[70px]">
         <div className="bg-primary text-white text-2xl px-4 py-5">
             {article.Headline}
         </div>
-        <div>
-            <span>Updated 06/15/2023 08:01 AM ET</span>
-        </div>
-        <div dangerouslySetInnerHTML={{ __html: article.ArticleContent }}>
+        <div className="p-5">
+            <div className="mb-[23px] ">
+                <span className="text-lg font-normal text-[333] italic">
+                    Updated 06/15/2023 08:01 AM ET
+                </span>
+            </div>
+            <div className="space-y-[23px]" 
+                dangerouslySetInnerHTML={{ __html: article.ArticleContent }}>
+            </div>
         </div>
     </div>)
 }
